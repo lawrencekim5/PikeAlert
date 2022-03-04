5a79c0c4b42d8e681303d20a77330d9954764984
Lawrence Kim <83670601+lawrencekim5@users.noreply.github.com>
2022-03-03 21:32:44 -0800

    Update PikeCheck.py

--- PikeCheck.py
+++ PikeCheck.py
@@ -9,19 +9,23 @@ client = commands.Bot(command_prefix = "&", intents = intents)
 ## Detecting Users Joining a Voice Call
 @client.event
 async def on_voice_state_update(member, before, after):
-    if not before.channel and after.channel and member.id == 260530976747159554: # Detects when the specified member joins the voice channel. Change member.id to the id of the Member to be detected (use developer mode in discord to copy ids).
+    if not before.channel and after.channel and member.id == <memberid>: # Detects when the specified member joins the voice channel. Set member.id to the id of the member to be detected (use developer mode in discord to copy ids).
 
-        channel = client.get_channel(868285868450922557) # Set the channel for alerts to be made in.
+        channel = client.get_channel(<channelid>) # Set the channel for alerts to be made in.
+        
+        # Customize responses or actions here.
         responses = [
                 f"@everyone PIKE DETECTED",
                 f"@everyone PIKE SIGHTED",
                 f"@everyone PIKE ALERT",
                 f"@everyone PIKE CHECK"
         ]
+  
         await channel.send(f"{random.choice(responses)}",file=discord.File(r'/home/voip/Desktop/PikeCheck/PikePlanted.wav'))
 
+# Bot commands using command_prefix. Not necessary.
 @client.command()
 async def test(ctx):
     await ctx.send(f"I'm working right now!")
 
-client.run("OTQ5MDE2NDM2MDU0MDY1MTky.YiEObg.KKTXIN2WgdRg2dnCA1bn6B4t4xc")
+client.run("<bot API token goes here>")
